0cb339e29aab4cb1333894259820829a8af9e6f7
Dima <livitorskipunovich@gmail.com>
2022-10-15 12:32:08 +0300

    Update: Add 'Завдання 3'

--- hw.py
+++ hw.py
@@ -1,6 +1,9 @@
-n1 = int(input(' число 1 '))
-n2 = int(input(' число 2 '))
-for i in range(1, 11):
-    for j in range(n1, n2+1):
-        print(f'{j} * {i} = {i * j}', end="\t")
-    print("")
+try:
+    n1 = int(input(' число 1 '))
+    n2 = int(input(' число 2 '))
+    for i in range(1, 11):
+        for j in range(n1, n2 + 1):
+            print(f'{j} * {i} = {i * j}', end="\t")
+        print("")
+except Exception as ex:
+   print(f'Erorr information: {ex}')
