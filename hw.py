123efbd36d39dab7a98330674edf04da6894802a
Dima <livitorskipunovich@gmail.com>
2022-10-15 12:35:00 +0300

    Update: Add 'Завдання 2'

--- hw.py
+++ hw.py
@@ -1,4 +1,7 @@
-for i in range(1, 11):
-    for j in range(1, 11):
-        print(f'{j} * {i} = {i * j}', end="\t")
-    print("\n")
+try:
+    for i in range(1, 11):
+        for j in range(1, 11):
+            print(f'{j} * {i} = {i * j}', end="\t")
+        print("\n")
+except Exception as ex:
+   print(f'Erorr information: {ex}')
